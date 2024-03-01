388255c312225d4d21802d59a1974d6f4962f80d
Hyun Soo An <ahs20103@gmail.com>
2024-03-01 15:49:35 +0900

    20240301

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -26,27 +26,32 @@ public class UIManager : MonoBehaviour {
     public GameObject gameoverUI; // 게임 오버시 활성화할 UI 
 
     // 탄약 텍스트 갱신
-    public void UpdateAmmoText(int magAmmo, int remainAmmo) {
+    public void UpdateAmmoText(int magAmmo, int remainAmmo) 
+    {
         ammoText.text = magAmmo + "/" + remainAmmo;
     }
 
     // 점수 텍스트 갱신
-    public void UpdateScoreText(int newScore) {
+    public void UpdateScoreText(int newScore) 
+    {
         scoreText.text = "Score : " + newScore;
     }
 
     // 적 웨이브 텍스트 갱신
-    public void UpdateWaveText(int waves, int count) {
+    public void UpdateWaveText(int waves, int count) 
+    {
         waveText.text = "Wave : " + waves + "\nEnemy Left : " + count;
     }
 
     // 게임 오버 UI 활성화
-    public void SetActiveGameoverUI(bool active) {
+    public void SetActiveGameoverUI(bool active) 
+    {
         gameoverUI.SetActive(active);
     }
 
     // 게임 재시작
-    public void GameRestart() {
+    public void GameRestart() 
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
